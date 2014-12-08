3f47b68ddabc502bbab49602b72edee73e877d27
Lance Albertson <lance@osuosl.org>
2014-12-08 15:26:05 -0800

    Bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures resource_from_hash'
 long_description 'Installs/Configures resource_from_hash'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'poise'
