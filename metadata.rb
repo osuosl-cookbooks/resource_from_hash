41a5884e98b06618121588393cbc3dc7b997c7cf
Jordan Evans <jordane@osuosl.org>
2015-10-23 13:17:15 -0700

    bump versions

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures resource_from_hash'
 long_description 'Installs/Configures resource_from_hash'
-version          '0.1.3'
+version          '0.1.4'
 
 depends 'poise', '> 2.0.0'
