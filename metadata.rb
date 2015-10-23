ab4b8035cbfe81fd06211d2d25beeb3c8c23dd95
Jordan Evans <jordane@osuosl.org>
2015-10-23 13:07:55 -0700

    update to support poise2

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures resource_from_hash'
 long_description 'Installs/Configures resource_from_hash'
 version          '0.1.3'
 
-depends 'poise', '< 2.0.0'
+depends 'poise', '> 2.0.0'
