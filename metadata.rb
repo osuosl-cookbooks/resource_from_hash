cd8107c54d34a8e8360ae487761dde12adb5f675
armiller <armiller@osuosl.org>
2016-05-06 16:04:02 -0700

    Bump cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures resource_from_hash'
 long_description 'Installs/Configures resource_from_hash'
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'poise', '> 2.0.0'
 
