57882ab86cc55b7844fa40eb754306a7376a6f09
Manatee <github@osuosl.org>
2018-08-21 19:47:40 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+1.0.0 (2018-08-21)
+------------------
+- Convert resource_from_hash to a custom resource and simplify
+
 # 0.1.5
 [#10](https://github.com/osuosl-cookbooks/resource_from_hash/pull/10) Add support
 for hash attributes
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures resource_from_hash'
 long_description 'Installs/Configures resource_from_hash'
-version          '0.1.5'
+version          '1.0.0'
 
 supports         'centos', '~> 6.0'
 supports         'centos', '~> 7.0'
