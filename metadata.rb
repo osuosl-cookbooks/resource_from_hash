94d5fa2ffa77c0c91de33c488bbbfefad6a151f3
Robert Detjens <detjensrobert@osuosl.org>
2019-12-22 18:01:56 -0800

    bump chef_version to >= 14.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 source_url       'https://github.com/osuosl-cookbooks/resource_from_hash'
 issues_url       'https://github.com/osuosl-cookbooks/resource_from_hash/issues'
 license          'Apache-2.0'
-chef_version     '>= 12.18' if respond_to?(:chef_version)
+chef_version     '>= 14.0'
 description      'Installs/Configures resource_from_hash'
 long_description 'Installs/Configures resource_from_hash'
 version          '1.1.0'
